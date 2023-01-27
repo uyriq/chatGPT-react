ec680246e72a67ef81e493e0be51b2375b5115f6
uyriq <105006434+uyriq@users.noreply.github.com>
2023-01-27 15:48:24 +0000

    "eslint"

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+
+    extends: ['react-app', 'prettier'],
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            rules: {},
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx', '.tsx', '.ts'] }],
+        'prettier/prettier': 0,
+        'linebreak-style': 0,
+
+        // Indent with 4 spaces
+        indent: ['off', 4],
+
+        // Indent JSX with 4 spaces
+        'react/jsx-indent': ['off', 4],
+
+        // Indent props with 4 spaces
+        'react/jsx-indent-props': ['off', 4],
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
+}
